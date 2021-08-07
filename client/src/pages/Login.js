e8a2cb37008683cbfdedca5004f3023e1bb96323
Trushil Budhia <trushil.budhia@gmail.com>
2021-08-07 20:18:05 +0800

    A.20.10 - Updated login and signup - added in login and signup pages

--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -1,5 +1,4 @@
 import React, { useState, useEffect } from 'react';
-import { Jumbotron, Container, Col, Form, Button, Card, CardColumns } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
 import Auth from '../utils/auth';
 import { useMutation } from '@apollo/client';
