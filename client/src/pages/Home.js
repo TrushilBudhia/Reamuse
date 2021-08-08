2f41e5de543426a6572ef7bce21b65aa16618652
Trushil Budhia <trushil.budhia@gmail.com>
2021-08-08 23:16:18 +0800

    A.20.10 - Updated styling for home, login and signup pages

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,25 +1,23 @@
-import React, { useState, useEffect } from 'react';
-import { Jumbotron, Container, Col, Form, Button, Card, CardColumns } from 'react-bootstrap';
+import React from 'react';
+import { Jumbotron, Container } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
 import Auth from '../utils/auth';
-import { useMutation } from '@apollo/client';
 import '../styles/style.css'
 
 const Home = () => {
   return (
     <>
-      <Jumbotron fluid className='text-light bg-navy'>
+      <Jumbotron fluid className='text-light bg-clear'>
         <Container className="welcome-page">
           <h1>Greetings Traveler</h1>
           <p className="welcome-paragraph">Welcome to the space of Remuse, where aliens have invaded and it is up to heroes like you to join the battle and defend against the enigmatic intentions of the space denizens through the challenge of puzzle games.</p>
           {!Auth.loggedIn() ? (
             <div>
-              <p>Please login or signup</p>
               <Link to="/login">
-                <Button className="mr-2">Login</Button>
+                <button className="button-style button-magenta mr-2">Login</button>
               </Link>
               <Link to="/signup">
-                <Button>Signup</Button>
+                <button className="button-style button-magenta">Signup</button>
               </Link>
             </div>
           ) : (
