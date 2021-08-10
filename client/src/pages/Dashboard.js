6a6ee9625ca741bf52d5e7fc5628141da6b25e9a
Trushil Budhia <trushil.budhia@gmail.com>
2021-08-10 23:40:32 +0800

    P.10.10 - Created the Dashboard page and styling for it, also created actions.js

---
+++ client/src/pages/Dashboard.js
@@ -0,0 +1,50 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import { useQuery, useMutation } from '@apollo/client';
+import Auth from '../utils/auth';
+import { GET_ME } from '../utils/queries';
+import '../styles/style.css'
+import { Header, MapButtons, RegionInfo, Title, TitleHeader, Description, DescriptionInner, Quote } from './Dashboard.styles.jsx'
+
+const Dashboard = () => {
+    // Setting up useQuery
+    const { loading, data } = useQuery(GET_ME);
+    const userData = data?.me || {};
+
+    // If data isn't here yet, display Loading
+    if (loading) {
+        return <h2>Loading...</h2>;
+    }
+
+    return (
+        <Header>
+            <div class="container">
+                <MapButtons>
+                    <RegionInfo>
+                        <Title>
+                            <TitleHeader>The world has been invaded!</TitleHeader>
+                        </Title>
+                        <Description>
+                            <DescriptionInner>
+                                Mysterious aliens have arrived on Earth and with their perplexing technologies, they have rendered humanities defenses ineffectual.
+                            </DescriptionInner>
+                            <DescriptionInner>
+                                The aliens have occupied six of the world's continents and with their abidance have issued a challenge to the world's inhabitants:
+                            </DescriptionInner>
+                            <Quote>
+                                "Partake in our activities of mental faculty if you wish to save this world"
+                            </Quote>
+                            <DescriptionInner>
+                                Help protect the world by overcoming the challenges the space denizens have set!
+                            </DescriptionInner>
+                        </Description>
+                        {/* <ButtonsContent></ButtonsContent> */}
+                    </RegionInfo>
+                    {/* <MapContent></MapContent> */}
+                </MapButtons>
+            </div>
+        </Header>
+    );
+};
+
+export default Dashboard;
